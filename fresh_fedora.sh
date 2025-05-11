7f52558e3d41103862f6790ff82a3965d848c26d
mlorenc <142215274+loureq177@users.noreply.github.com>
2025-05-11 21:56:43 +0200

    version 2

--- fresh_fedora.sh
+++ fresh_fedora.sh
@@ -21,23 +21,34 @@ dnf upgrade -y
 # Install essential apps
 dnf install -y \
 thunderbird\
-gimp
+gimp\
+zsh\
+vim
 
-flatpak install -y com.mattjakeman.ExtensionManager
-
+flatpak install -y\
+com.mattjakeman.ExtensionManager\
+flathub com.discordapp.Discord\
+flathub com.spotify.Client
 
 # Install Visual Studio Code via dnf
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]
 name=Visual Studio Code
 baseurl=https://packages.microsoft.com/yumrepos/vscode
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf install code
+dnf install code
 
 # Fix brightness issue
 gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
+chsh -s /usr/bin/zsh
+
 dnf autoremove -y
 dnf clean all -y
+
+# Configure git profile
+git config --global user.name mlorenc
+git config --global user.email 142215274+loureq177@users.noreply.github.com
+git config --global init.defaultBranch main
