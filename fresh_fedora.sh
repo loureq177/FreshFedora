0858b12532b9e79a2f3618ae3b4c4a6976daad80
mlorenc <142215274+loureq177@users.noreply.github.com>
2025-05-27 11:04:44 +0200

    used my other dotfiles repo to clone some config files

--- fresh_fedora.sh
+++ fresh_fedora.sh
@@ -2,6 +2,8 @@
 
 # This script has to be run with superuser privileages.
 
+# Install Z-Shell
+######################################
 
 # Remove unnecessary stuff
 sudo dnf remove -y \
@@ -20,7 +22,7 @@ gnome-tweaks
 dnf upgrade -y
 
 # Install essential apps from dnf
-dnf install -y \
+dnf install -y --skip-unavailable\
 thunderbird\
 gimp\
 zsh\
@@ -46,11 +48,10 @@ kernel-devel\
 # System utilities
 xrandr
 
-
+# Flatpak apps
 flatpak install -y\
-com.mattjakeman.ExtensionManager\
+flathub com.mattjakeman.ExtensionManager\
 flathub com.discordapp.Discord\
-flathub com.spotify.Client
 
 # Install Visual Studio Code via dnf
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -62,13 +63,20 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf install code
 
+# Clone .dotfiles from my other repository
+git clone https://github.com/loureq177/dotfiles ~/.dotfiles
+
+# Create symlinks to those .files
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
 # Fix brightness issue
 gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
 # Fix lofree keyboard
-echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+echo 2 | tee /sys/module/hid_apple/parameters/fnmode
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="hid_apple.fnmode=2 /' /etc/default/grub
-chsh -s /usr/bin/zsh
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # Tailor settings
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
@@ -76,8 +84,3 @@ gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 dnf autoremove -y
 dnf clean all -y
-
-# Configure git profile
-git config --global user.name mlorenc
-git config --global user.email 142215274+loureq177@users.noreply.github.com
-git config --global init.defaultBranch main
